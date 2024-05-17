a6c459d6e30525d65a3e8746bde8e2d7c97bdcd0
takaya-ishiyama <takaya_ishiyama0810@yahoo.co.jp>
2024-05-17 13:21:26 +0900

    a

--- apps/frontend/app/routes/organization.$id/route.tsx
+++ apps/frontend/app/routes/organization.$id/route.tsx
@@ -1,40 +1,37 @@
 import React from "react";
 import { RequestOptions, gql } from "graphql-request";
-import { GetOrganizationDocument, GetOrganizationQueryVariables, GetOrganizationQuery, } from "app/infrastructure/graphql";
+import {
+  GetOrganizationQueryVariables,
+  GetOrganizationQuery,
+  GetOrganizationDocument,
+} from "app/infrastructure/graphql";
 import { client } from "app/infrastructure";
-import { json } from "@remix-run/node";
-import { useLoaderData, useParams } from "@remix-run/react";
-import { useGetOrganization } from "./hooks";
+import { useLoaderData } from "@remix-run/react";
+import { LoaderFunction } from "@remix-run/node";
 
-const query = gql`
-query getOrganization($id: ID!){
-  findOne(id: $id){
-    id
-    name
-  }
-} 
-`
-export async function loader() {
+export const loader: LoaderFunction = async ({ params }) => {
+  if (params.id == null) return
   const requestOptions: RequestOptions<
     GetOrganizationQueryVariables,
     GetOrganizationQuery
   > = {
     document: GetOrganizationDocument,
-    variables: { id: "5cda43e9-abfe-4ddd-800c-c1a8dedb4bcf" },
+    variables: { id: params.id },
+    // variables: { id: "5cda43e9-abfe-4ddd-800c-c1a8dedb4bcf" },
   };
-  const params = useParams();
   const { findOne } = await client.request(requestOptions);
-  return { organization: findOne }
+  return { organization: findOne };
 };
-
 
 const OrganizationScreen: React.FC = () => {
   const { organization } = useLoaderData<typeof loader>();
 
+  console.log("\n-----------------------\n", organization);
+
   return (
     <div>
-      <div>aaaa</div >
+      <div>aaaa</div>
     </div>
-  )
-}
+  );
+};
 export default OrganizationScreen;
--- apps/frontend/app/routes/top/route.tsx
+++ apps/frontend/app/routes/top/route.tsx
@@ -4,16 +4,19 @@ import React from "react";
 
 const TopScreen = () => {
   const navigate = useNavigate();
-  const [organizationId, setOrganizationId] = React.useState<string | null>("5cda43e9-abfe-4ddd-800c-c1a8dedb4bcf")
+  const [organizationId, setOrganizationId] = React.useState<string | null>(
+    "5cda43e9-abfe-4ddd-800c-c1a8dedb4bcf",
+  );
 
   const onClick = React.useCallback(() => {
-    if (organizationId === null) return
-    navigate(RouteName.organization(organizationId))
-  }, [organizationId, navigate, RouteName.organization])
+    if (organizationId === null) return;
+    navigate(RouteName.organization(organizationId));
+  }, [organizationId, navigate]);
+
   return (
     <div>
-      <button onClick={onClick} >to organization</button>
+      <button onClick={onClick}>to organization</button>
     </div>
-  )
-}
+  );
+};
 export default TopScreen;
