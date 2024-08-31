cdd2d74147d96a35af7d9dcd52e608b6ff331701
takaya-ishiyama <takaya_ishiyama0810@yahoo.co.jp>
2024-08-31 22:42:10 +0900

    a

--- apps/backend/infrastructure/src/repository/mod.rs
+++ apps/backend/infrastructure/src/repository/mod.rs
@@ -1,3 +1,4 @@
 pub mod item_repository;
 pub mod organization_repository;
+pub mod organization_repository_test;
 pub mod repository_impl;
---
+++ apps/backend/infrastructure/src/repository/organization_repository_test.rs
@@ -0,0 +1,47 @@
+#[cfg(test)]
+mod tests {
+
+    use std::sync::Arc;
+
+    use domain::{
+        infrastructure::interface::repository::organization_repository_interface::OrganizationRepository,
+        value_object::organaization::organization_type::ORGANIZATION_TYPE,
+    };
+    use sqlx::PgPool;
+
+    use crate::{
+        repository::organization_repository::OrganizationRepositoryImpl,
+        test::{setup_testdb::setup_database, stb_data::stb_organization},
+    };
+
+    #[sqlx::test]
+    async fn test_create_true_response(pool: PgPool) -> sqlx::Result<()> {
+        setup_database(&pool).await;
+        let db = Arc::new(pool);
+        let repo = OrganizationRepositoryImpl::new(db);
+
+        let ognz_type = ORGANIZATION_TYPE::PUBLIC;
+        let organization = repo.create("test", &ognz_type, Some("")).await.unwrap();
+
+        assert_eq!(organization.get_params().name, "test".to_string());
+
+        Ok(())
+    }
+
+    #[sqlx::test]
+    async fn test_find_one(pool: PgPool) -> sqlx::Result<()> {
+        setup_database(&pool).await;
+        let stb = stb_organization();
+        let db = Arc::new(pool);
+        let repo = OrganizationRepositoryImpl::new(db);
+
+        let organization = repo
+            .find_one_by_id("17b5ac0c-1429-469a-8522-053f7bf0f80d")
+            .await
+            .unwrap();
+
+        assert_eq!(organization.get_params().id, stb[0].get_params().id);
+
+        Ok(())
+    }
+}
